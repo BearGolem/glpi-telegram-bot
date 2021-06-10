32a114c3c4870822df41df0bd2a078c363172946
Alex <xenon_13@mail.ru>
2021-06-10 09:30:16 +0300

    Menu update
    Changed greeting and help messeges

---
+++ bot/app/generic/onboarding_v2.py
@@ -0,0 +1,70 @@
+"""Login proccess
+"""
+import logging
+from aiogram.dispatcher import FSMContext
+import bot.app.core as core
+from bot.usersession import UserSession
+from bot.glpi_api import GLPIError
+from bot.app.bot_state import Form
+from bot.app.keyboard import select_command, no_keyboard
+
+
+async def onboarding_start(user_id: int) -> None:
+    """
+    Conversation's entry point
+    """
+    logging.info("%d started onboarding", user_id)
+    # Set state
+    await Form.to_enter_login.set()
+    await core.bot.send_message(
+        user_id,
+        "Введите Ваш логин",
+        reply_markup=no_keyboard,
+    )
+
+
+async def process_cancel(user_id: int, state: FSMContext, info_text: str = "") -> None:
+    """Reset state
+
+    Args:
+        user_id (int): telegram user id that issued command
+    """
+    logging.info("User ID %d reset state", user_id)
+    await UserSession(user_id=user_id).destroy(state=state)
+    # await state.finish()
+    if len(info_text) > 0:
+        await core.bot.send_message(user_id, info_text)
+
+
+async def process_to_enter_login(user_id: int, login: str, state: FSMContext) -> None:
+    """
+    Process user login
+    """
+    logging.info("%d in to_enter_login", user_id)
+
+    await UserSession(user_id=user_id).create(state=state, login=login)
+
+    await Form.to_enter_password.set()
+    await core.bot.send_message(user_id, "Введите Ваш пароль", reply_markup=no_keyboard)
+
+
+async def process_to_enter_password(
+    user_id: int, password: str, state: FSMContext
+) -> None:
+    """
+    Process user password
+    """
+    try:
+        await UserSession(user_id=user_id).create(state=state, password=password)
+    except GLPIError as err:
+        logging.info(
+            "User ID %d tried to login to glpi. Error: %s", user_id, err)
+        await process_cancel(
+            user_id,
+            state,
+            "Неправильная пара логин/пароль. Давайте попробуем еще раз. Введите /start",
+        )
+        return
+    logging.info("User ID %d has logged in to glpi", user_id)
+    await Form.logged_in.set()
+    await core.bot.send_message(user_id, "Авторизация прошла успешно, теперь вы можете \033[4m/Создать заявку\033[0m и посмотреть \033[4m/Мои заявки\033[0m. Для просмотра всех команд введите \033[4m/Help\033[0m.", reply_markup=select_command)
